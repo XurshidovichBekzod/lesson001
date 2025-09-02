9d3d6d741b5880a0ff1a23883e4786d4160c8b7c
XurshidovichBekzod <xurshidovichbekzod@gmail.com>
2025-09-02 19:44:51 +0500

    lesson001

--- src/app/comments/page.tsx
+++ src/app/comments/page.tsx
@@ -1,10 +1,32 @@
 import Header from '@/components/Header'
 import React from 'react'
 
-const Comments = () => {
+interface IComments{
+  id: string
+  body: string
+  likes: number
+  postId: number
+}
+
+const Comments = async() => {
+  const res = await fetch("https://dummyjson.com/comments")
+  const data = await res.json()
+  console.log(data);
+  
   return (
     <>
-    <div>Comments</div>
+    <div className='container mx-auto grid grid-cols-4 gap-[16px]'>
+      {
+        data?.comments?.map((comments: IComments)=>(
+          <div className='container ml-[10px] mb-[30px] mt-[30px] w-[345px] h-[150px] bg-[#1e1e1e] text-white pt-[10px] rounded-[3px] border pl-[20px]' key={comments.id}>
+          <h1 className='line-clamp-1 mb-[10px] text-[20px] font-bold'>{comments.body}</h1>            
+          <p><span className='text-red-800'>Like:</span> {comments.likes}</p>
+          <p><span className='text-red-800'>Post:</span> {comments.postId}</p>
+
+          </div>
+        ))
+      }
+    </div>
     </>
   )
 }
--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,13 +1,38 @@
 import Header from '@/components/Header'
+
+interface IRecipes{
+  id: string
+  name: string
+  cuisine: string
+  rating: number
+  mealType: string
+}
+
 import React from 'react'
 
 const Recipes = async() => {
- 
+  const res = await fetch("https://dummyjson.com/recipes")
+  const data = await res.json()
+  console.log(data);
+  
 
   return (
     <>
     
-    <div>Recipes</div>
+    <div className='container mx-auto grid grid-cols-4 gap-[16px]'>
+      {
+        data?.recipes?.map((recipes: IRecipes) => (
+          <div className='container ml-[10px] mb-[30px] mt-[30px] w-[345px] h-[150px] bg-[#1e1e1e] text-white pt-[10px] rounded-[3px] border pl-[20px]' key={recipes.id}>
+            <div className='line-clamp-1 mb-[10px] text-[20px] font-bold'>{recipes.name}</div>
+            <p><span className='text-red-800'>Cuisine:</span> {recipes.cuisine}</p>
+            <div>
+              <p className='text-red-800'>{recipes.rating}</p>
+              <p>{recipes.mealType}</p>
+            </div>
+          </div>
+        ))
+      }
+    </div>
     </>
     
   )
