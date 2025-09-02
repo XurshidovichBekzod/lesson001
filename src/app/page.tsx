275dca3a236c6cfc55c3160a34cd450c1d28495a
XurshidovichBekzod <xurshidovichbekzod@gmail.com>
2025-09-02 19:46:33 +0500

    homework

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
 import Image from "next/image";
+import Products from "./products/page";
 
 export default function Home() {
   return (
     <div>
+      <Products></Products>
     </div>
   );
 }
