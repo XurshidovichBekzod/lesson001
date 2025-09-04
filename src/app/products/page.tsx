4d7f38a2c1a977663c1e22dad5d34d47f143f9c7
XurshidovichBekzod <xurshidovichbekzod@gmail.com>
2025-09-04 16:20:07 +0500

    lesson

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,9 @@
 import Header from '@/components/Header'
+import Image from 'next/image'
 import React from 'react'
 
 interface IProducts{
+  thumbnail: string
   id: string
   brand: string
   description: string
@@ -20,7 +22,8 @@ const Products = async() => {
       <div className='container mx-auto grid grid-cols-4 gap-[16px]'>
         {
           data?.products?.map((products: IProducts) => (
-            <div className='container ml-[10px] mb-[30px] mt-[30px] w-[345px] h-[150px] bg-[#1e1e1e] text-white pt-[10px] rounded-[3px] border pl-[20px]' key={products.id}>
+            <div className='container ml-[10px] mb-[30px] pb-[30px] mt-[30px] w-[345px]  bg-[#1e1e1e] text-white pt-[10px] rounded-[3px] border pl-[20px]' key={products.id}>
+              <Image width={300} height={300} src={products.thumbnail} alt={products.brand}/>
               <h1 className='line-clamp-1 mb-[10px] text-[20px] font-bold'>{products.brand || "No brand"}</h1>
               <p className='line-clamp-2 mb-[15px]'>{products.description}</p>
               <div className='flex justify-between mr-[20px] text-red-800'>
--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,6 +1,8 @@
 import Header from '@/components/Header'
+import Image from 'next/image'
 
 interface IRecipes{
+  image: string
   id: string
   name: string
   cuisine: string
@@ -22,7 +24,8 @@ const Recipes = async() => {
     <div className='container mx-auto grid grid-cols-4 gap-[16px]'>
       {
         data?.recipes?.map((recipes: IRecipes) => (
-          <div className='container ml-[10px] mb-[30px] mt-[30px] w-[345px] h-[150px] bg-[#1e1e1e] text-white pt-[10px] rounded-[3px] border pl-[20px]' key={recipes.id}>
+          <div className='container ml-[10px] mb-[30px] mt-[30px] w-[345px] pb-[30px] bg-[#1e1e1e] text-white pt-[10px] rounded-[3px] border pl-[20px]' key={recipes.id}>
+            <Image width={300} height={300} src={recipes.image} alt={recipes.name}/>
             <div className='line-clamp-1 mb-[10px] text-[20px] font-bold'>{recipes.name}</div>
             <p><span className='text-red-800'>Cuisine:</span> {recipes.cuisine}</p>
             <div>
